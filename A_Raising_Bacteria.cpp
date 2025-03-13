03ccda4a466c3ab0c997668d7f1fa0b23b5f54b2
biswass101 <biswassnaeemcse@gmail.com>
2025-03-13 23:08:55 +0600

    bacteria problem added

--- A_Raising_Bacteria.cpp
+++ A_Raising_Bacteria.cpp
@@ -3,9 +3,12 @@ using namespace std;
 int main()
 {
     int n; cin >> n;
-    if(n & !(n & n - 1)) cout << 1 << endl;
-    else {
-        
+    int b = 0;
+    for(int i = 0; i < 31; i++) {
+        int bit = n & (1 << i);
+        if(bit != 0) b++;
     }
+
+    cout << b << endl;
     return 0;
 }
